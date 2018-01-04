fca3d82f0c02b9cb4c911c956727156de295c653
Abinash <abinash.shrestha09@gmail.com>
2018-01-04 12:31:58 +1100

    DV-4-b minor fix

--- header-custom.php
+++ header-custom.php
@@ -5,6 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Heebo:300,400,500,700" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory() . '/dist/ad.css' ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/dist/ad.css'; ?>">
 </head>
 <body>
