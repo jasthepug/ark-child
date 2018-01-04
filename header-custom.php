aa677655ced92a2b2088f34ad0e0dfc5a9f2de61
Abinash <abinash.shrestha09@gmail.com>
2018-01-04 12:20:15 +1100

    DV-4 added bootstrap

--- header-custom.php
+++ header-custom.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
     <link href="https://fonts.googleapis.com/css?family=Heebo:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/dist/ad.css' ?>">
 </head>
 <body>
