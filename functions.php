9c36967cc56d99ca2d4ca2ae0ea70c5b6d70cb90
Abinash <abinash.shrestha09@gmail.com>
2018-01-11 17:15:53 +1100

    DV-5 ad pages wordpress UI created

--- functions.php
+++ functions.php
@@ -1,59 +1,205 @@
 <?php
 
+/**
+ * Include and setup custom metaboxes and fields. (make sure you copy this file to outside the CMB2 directory)
+ *
+ * Be sure to replace all instances of 'yourprefix_' with your project's prefix.
+ * http://nacin.com/2010/05/11/in-wordpress-prefix-everything/
+ *
+ * @category YourThemeOrPlugin
+ * @package  Demo_CMB2
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link     https://github.com/CMB2/CMB2
+ */
+
+add_action('cmb2_admin_init', 'stores_ad_group');
+add_action('cmb2_admin_init', 'online_store_ad_group');
+add_action('cmb2_admin_init', 'trademark_text');
+/**
+ * Hook in and add a metabox to demonstrate repeatable grouped fields
+ */
+function stores_ad_group()
+{
+    $prefix = 'stores_ad_';
+
+    /**
+     * Repeatable Field Groups
+     */
+    $cmb_group = new_cmb2_box(array(
+        'id' => $prefix . 'metabox',
+        'title' => esc_html__('Stores', 'cmb2'),
+        'object_types' => array('page'),
+    ));
+
+    // $group_field_id is the field id string, so in this case: $prefix . 'demo'
+    $group_field_id = $cmb_group->add_field(array(
+        'id' => $prefix . 'demo',
+        'type' => 'group',
+        'description' => esc_html__('Entries for stores', 'cmb2'),
+        'options' => array(
+            'group_title' => esc_html__('Entry {#}', 'cmb2'), // {#} gets replaced by row number
+            'add_button' => esc_html__('Add Another Entry', 'cmb2'),
+            'remove_button' => esc_html__('Remove Entry', 'cmb2'),
+            'sortable' => true, // beta
+            // 'closed'     => true, // true to have the groups closed by default
+        ),
+    ));
+
+    /**
+     * Group fields works the same, except ids only need
+     * to be unique to the group. Prefix is not needed.
+     *
+     * The parent field's id needs to be passed as the first argument.
+     */
+    $cmb_group->add_group_field($group_field_id, array(
+        'name' => esc_html__('Entry Title', 'cmb2'),
+        'id' => 'title',
+        'type' => 'text',
+        // 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+    ));
+
+    $cmb_group->add_group_field($group_field_id, array(
+        'name' => esc_html__('Entry Image', 'cmb2'),
+        'id' => 'image',
+        'type' => 'file',
+    ));
+}
+
+function online_store_ad_group() {
+    $prefix = 'online_stores_';
+
+    /**
+     * Repeatable Field Groups
+     */
+    $cmb_group = new_cmb2_box( array(
+        'id'           => $prefix . 'metabox',
+        'title'        => esc_html__( 'Online Store Links', 'cmb2' ),
+        'object_types' => array( 'page' ),
+    ) );
+
+    // $group_field_id is the field id string, so in this case: $prefix . 'demo'
+    $group_field_id = $cmb_group->add_field( array(
+        'id'          => $prefix . 'demo',
+        'type'        => 'group',
+        'description' => esc_html__( 'Entries for online stores', 'cmb2' ),
+        'options'     => array(
+            'group_title'   => esc_html__( 'Entry {#}', 'cmb2' ), // {#} gets replaced by row number
+            'add_button'    => esc_html__( 'Add Another Entry', 'cmb2' ),
+            'remove_button' => esc_html__( 'Remove Entry', 'cmb2' ),
+            'sortable'      => true, // beta
+            // 'closed'     => true, // true to have the groups closed by default
+        ),
+    ) );
+
+    $cmb_group->add_group_field($group_field_id, array(
+        'name' => __( 'Website URL', 'cmb2' ),
+        'id'   => 'online_store_url',
+        'type' => 'text_url',
+        // 'protocols' => array( 'http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet' ), // Array of allowed protocols
+    ) );
+
+    /**
+     * Group fields works the same, except ids only need
+     * to be unique to the group. Prefix is not needed.
+     *
+     * The parent field's id needs to be passed as the first argument.
+     */
+    $cmb_group->add_group_field( $group_field_id, array(
+        'name'       => esc_html__( 'Entry Title', 'cmb2' ),
+        'id'         => 'title',
+        'type'       => 'text',
+        // 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+    ) );
+
+    $cmb_group->add_group_field( $group_field_id, array(
+        'name' => esc_html__( 'Entry Image', 'cmb2' ),
+        'id'   => 'image',
+        'type' => 'file',
+    ) );
+
+}
+
+function trademark_text() {
+
+    $cmb = new_cmb2_box( array(
+        'id'            => $prefix . 'metabox',
+        'title'         => esc_html__( 'Test Metabox', 'cmb2' ),
+        'object_types'  => array( 'page' ), // Post type
+        // 'show_on_cb' => 'yourprefix_show_if_front_page', // function should return a bool value
+        // 'context'    => 'normal',
+        // 'priority'   => 'high',
+        // 'show_names' => true, // Show field names on the left
+        // 'cmb_styles' => false, // false to disable the CMB stylesheet
+        // 'closed'     => true, // true to keep the metabox closed by default
+        // 'classes'    => 'extra-class', // Extra cmb2-wrap classes
+        // 'classes_cb' => 'yourprefix_add_some_classes', // Add classes through a callback.
+    ) );
+
+    $cmb->add_field( array(
+        'name'    => 'Trademark text',
+        'desc'    => 'Trademark text here',
+        'default' => 'standard value (optional)',
+        'id'      => 'trademark_text',
+        'type'    => 'text',
+    ) );
+}
+
 add_action('wp_enqueue_scripts', 'ark_register_styles');
 /**
  * Enqueues styles.
  */
 
-function ark_register_styles() {
+function ark_register_styles()
+{
 
-	// Bootstrap
-	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/plugins/bootstrap/css/bootstrap.min.css', array(), '3.3.6' );
+    // Bootstrap
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/plugins/bootstrap/css/bootstrap.min.css', array(), '3.3.6');
 
-	// Theme javascript plugins
-	wp_enqueue_style('jquery.mCustomScrollbar', get_template_directory_uri() . '/assets/plugins/scrollbar/jquery.mCustomScrollbar.css', array(), '3.1.12' );
-	wp_enqueue_style('owl.carousel', get_template_directory_uri() . '/assets/plugins/owl-carousel/assets/owl.carousel.css', array(), '1.3.2' );
-	wp_enqueue_style('animate', get_template_directory_uri() . '/assets/css/animate.css', array(), '3.5.1' );
-	wp_enqueue_style('magnific-popup', get_template_directory_uri() . '/assets/plugins/magnific-popup/magnific-popup.css', array(), '1.1.0' );
-	wp_enqueue_style('cubeportfolio', get_template_directory_uri() . '/assets/plugins/cubeportfolio/css/cubeportfolio.min.css', array(), '3.8.0' );
+    // Theme javascript plugins
+    wp_enqueue_style('jquery.mCustomScrollbar', get_template_directory_uri() . '/assets/plugins/scrollbar/jquery.mCustomScrollbar.css', array(), '3.1.12');
+    wp_enqueue_style('owl.carousel', get_template_directory_uri() . '/assets/plugins/owl-carousel/assets/owl.carousel.css', array(), '1.3.2');
+    wp_enqueue_style('animate', get_template_directory_uri() . '/assets/css/animate.css', array(), '3.5.1');
+    wp_enqueue_style('magnific-popup', get_template_directory_uri() . '/assets/plugins/magnific-popup/magnific-popup.css', array(), '1.1.0');
+    wp_enqueue_style('cubeportfolio', get_template_directory_uri() . '/assets/plugins/cubeportfolio/css/cubeportfolio.min.css', array(), '3.8.0');
 
-	// Icon Fonts
-	if ( class_exists('ffContainer') ) {
-		ark_wp_enqueue_framework_icon_fonts_styles();
-	}else{
-		wp_enqueue_style('ark-modified-font-awesome', get_template_directory_uri() . '/no-ff/iconfonts/ff-font-awesome4/ff-font-awesome4.css' );
-		wp_enqueue_style('ark-modified-font-et-line', get_template_directory_uri() . '/no-ff/iconfonts/ff-font-et-line/ff-font-et-line.css' );
-	}
+    // Icon Fonts
+    if (class_exists('ffContainer')) {
+        ark_wp_enqueue_framework_icon_fonts_styles();
+    } else {
+        wp_enqueue_style('ark-modified-font-awesome', get_template_directory_uri() . '/no-ff/iconfonts/ff-font-awesome4/ff-font-awesome4.css');
+        wp_enqueue_style('ark-modified-font-et-line', get_template_directory_uri() . '/no-ff/iconfonts/ff-font-et-line/ff-font-et-line.css');
+    }
 
-	// Theme Styles
-	wp_enqueue_style('ark-one-page-business', get_template_directory_uri() . '/assets/css/one-page-business.css' );
-	wp_enqueue_style('ark-landing', get_template_directory_uri() . '/assets/css/landing.css' );
+    // Theme Styles
+    wp_enqueue_style('ark-one-page-business', get_template_directory_uri() . '/assets/css/one-page-business.css');
+    wp_enqueue_style('ark-landing', get_template_directory_uri() . '/assets/css/landing.css');
 
-	// Stylesheet from the PARENT theme
-	wp_enqueue_style('ark-style', get_template_directory_uri().'/style.css' );
+    // Stylesheet from the PARENT theme
+    wp_enqueue_style('ark-style', get_template_directory_uri() . '/style.css');
 
-	// Stylesheet from the CHILD theme
-	wp_enqueue_style('ark-style-child', get_stylesheet_directory_uri().'/style.css' );
+    // Stylesheet from the CHILD theme
+    wp_enqueue_style('ark-style-child', get_stylesheet_directory_uri() . '/style.css');
 
-	if ( class_exists('ffContainer') ) {
-		ark_wp_enqueue_theme_google_fonts_styles();
-	}else{
-		wp_enqueue_style( 'ark-fonts', ark_fonts_url(), array(), null );
-		wp_enqueue_style( 'ark-fonts-family', get_template_directory_uri() . '/no-ff/no-ff-fonts.css' );
-	}
+    if (class_exists('ffContainer')) {
+        ark_wp_enqueue_theme_google_fonts_styles();
+    } else {
+        wp_enqueue_style('ark-fonts', ark_fonts_url(), array(), null);
+        wp_enqueue_style('ark-fonts-family', get_template_directory_uri() . '/no-ff/no-ff-fonts.css');
+    }
 
-	if( class_exists('WooCommerce') ) {
-		wp_enqueue_style('ark-woocommerce', get_template_directory_uri() . '/woocommerce/woocommerce.css');
-	}
-	$url = ark_get_custom_color_css_url();
-	wp_enqueue_style('ark-colors', $url );
+    if (class_exists('WooCommerce')) {
+        wp_enqueue_style('ark-woocommerce', get_template_directory_uri() . '/woocommerce/woocommerce.css');
+    }
+    $url = ark_get_custom_color_css_url();
+    wp_enqueue_style('ark-colors', $url);
 
-	// TwentyTwenty
-	wp_enqueue_style('twentytwenty', get_template_directory_uri() . '/assets/plugins/twentytwenty/css/twentytwenty.css', array() );
+    // TwentyTwenty
+    wp_enqueue_style('twentytwenty', get_template_directory_uri() . '/assets/plugins/twentytwenty/css/twentytwenty.css', array());
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles');
+add_action('wp_enqueue_scripts', 'child_enqueue_styles');
 
-function child_enqueue_styles() {
-    wp_enqueue_style( 'ad-style', get_template_directory_uri() . '/dist/ad.css', array());
+function child_enqueue_styles()
+{
+    wp_enqueue_style('ad-style', get_template_directory_uri() . '/dist/ad.css', array());
 }
--- snorel-ad.php
+++ snorel-ad.php
@@ -7,67 +7,95 @@
 
 get_header('custom'); ?>
 <div class="container page-container">
-    <div class="brand-info row">
+    <section class="brand-info row">
         <div class="col-xs-12 col-sm-2">
             <div class="text-center">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Pill_Bottle.svg/2000px-Pill_Bottle.svg.png"
-                     class="brand-image" alt="">
+                <?php
+                if (has_post_thumbnail()):
+                    echo get_the_post_thumbnail($post, 'large', array('class' => 'brand-image'));
+                endif;
+                ?>
             </div>
         </div>
         <div class="col-xs-12 col-sm-4">
-            <h1 class="brand-title heading">Snoring Relief</h1>
-            <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum consequatur minus
-                doloremque amet enim molestiae minima sequi consequuntur cupiditate officia beatae, facere recusandae a
-                natus perspiciatis ex? Sed, autem aut.</p>
+            <?php
+            while (have_posts()) : the_post();
+                echo the_content();
+            endwhile; ?>
         </div>
-    </div>
-    <div class="row availability">
+    </section>
+    <section class="row availability">
         <div class="col-xs-12">
             <h2 class="heading">Available at:</h2>
         </div>
-        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
-            <img src="https://thesquaremirrabooka.com.au/wp-content/uploads/2015/09/priceline.png"
-                 class="img-responsive" alt="">
-            <span class="store-name">Priceline</span>
-        </div>
-        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
-            <img src="https://thesquaremirrabooka.com.au/wp-content/uploads/2015/09/priceline.png"
-                 class="img-responsive" alt="">
-            <span class="store-name">Priceline</span>
-        </div>
-        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
-            <img src="https://thesquaremirrabooka.com.au/wp-content/uploads/2015/09/priceline.png"
-                 class="img-responsive" alt="">
-            <span class="store-name">Priceline</span>
-        </div>
-    </div>
-    <div class="row availability">
+        <?php while (have_posts()) : the_post(); ?>
+            <?php
+            $entries_store = get_post_meta(get_the_ID(), 'stores_ad_demo', true);
+
+            foreach ((array)$entries_store as $key => $entry) {
+
+                $img = $title = $desc = $caption = '';
+
+                if (isset($entry['title'])) {
+                    $title = esc_html($entry['title']);
+                }
+
+                if (isset($entry['image_id'])) {
+                    $img = wp_get_attachment_image($entry['image_id'], 'medium', null, array(
+                        'class' => 'img-responsive',
+                    ));
+                }
+
+                // Do something with the data
+                ?>
+                <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
+                    <?php echo $img; ?>
+                    <span class="store-name"><?php echo $title; ?></span>
+                </div>
+                <?php
+            }
+            ?>
+        <?php endwhile; // end of the loop. ?>
+    </section>
+    <section class="row availability">
         <div class="col-xs-12">
             <h2 class="heading border-top border-top--dashed">Available Online at:</h2>
         </div>
-        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
-            <a class="store-link" href="#1">
-                <img src="https://thesquaremirrabooka.com.au/wp-content/uploads/2015/09/priceline.png"
-                     class="img-responsive" alt="">
-                <span class="store-name">Priceline</span>
-            </a>
-        </div>
-        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
-            <a class="store-link" href="#2">
-                <img src="https://thesquaremirrabooka.com.au/wp-content/uploads/2015/09/priceline.png"
-                     class="img-responsive" alt="">
-                <span class="store-name">Priceline</span>
-            </a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-            <p class="text-muted disclaimer border-top"><span class="text-uppercase">Disclaimer:</span> Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quam, quas. Ipsam, illum architecto? Doloremque labore
-                iusto provident dolores! Quos, quo. Illo accusamus esse error? Provident temporibus id iusto reiciendis
-                repudiandae.</p>
-        </div>
-    </div>
-</div>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php
+            $entries_online_store = get_post_meta(get_the_ID(), 'online_stores_demo', true);
 
-<?php get_footer('custom'); ?>
+            foreach ((array)$entries_online_store as $key => $entry) {
+
+                $img = $title = $desc = $caption = '';
+
+                if (isset($entry['title'])) {
+                    $title = esc_html($entry['title']);
+                }
+
+                if (isset($entry['online_store_url'])) {
+                    $online_store_ad_link = esc_html($entry['online_store_url']);
+                }
+
+                if (isset($entry['image_id'])) {
+                    $img = wp_get_attachment_image($entry['image_id'], 'medium', null, array(
+                        'class' => 'img-responsive',
+                    ));
+                }
+
+                $caption = isset($entry['image_caption']) ? wpautop($entry['image_caption']) : '';
+
+                // Do something with the data
+                ?>
+                <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-4">
+                    <a class="store-link" href="<?php echo $online_store_ad_link; ?>" target="_blank">
+                        <?php echo $img; ?>
+                        <span class="store-name"><?php echo $title; ?></span>
+                    </a>
+                </div>
+                <?php
+            }
+            ?>
+        <?php endwhile; // end of the loop. ?>
+    </section>
+    <?php get_footer('custom'); ?>
